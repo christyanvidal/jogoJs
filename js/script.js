822326c6a0a1a452d93ea0da5aa926a3cf979118
Christyan Vidal <christyanbjj@gmail.com>
2023-04-24 23:12:46 -0300

    jogo js
    altereção no js 1

--- js/script.js
+++ js/script.js
@@ -5,11 +5,8 @@ const jump = () => {
     mario.classList.add(jump);
 
     serTimeout(() => {
-
-        mario.classList.remove('jump');
-
+    mario.classList.remove('jump');
     }, 500);
-
 }
 
 const loop = setInterval(() => {
@@ -17,11 +14,9 @@ const loop = setInterval(() => {
     console.log('loop')
 
     const pipePosition = pipe.offsetLeft;
-    const marioPositon = +window.getComputedStyle(mario).bottom.replace('pipe')
+    const marioPositon = +window.getComputedStyle(mario).bottom.replace('px', '');
  
-    console.log(marioPosition)
-
-    if (pipePosition <= 120 && pipePosition > 0 && marioPositon <80) {
+    if (pipePosition <- 120 && pipePosition > 0 && marioPositon <80) {
 
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`;
@@ -29,7 +24,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPositon}`;
 
-        mario.src='/img/game-over.png';
+        mario.src='./img/game-over.png';
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
 
