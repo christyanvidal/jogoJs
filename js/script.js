c1424a7dcbd2b5255bab7023fd1d36996c646ed5
Glaicon Marques <glaicon.marques@gmail.com>
2023-04-24 23:23:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 
 const jump = () => {
-    mario.classList.add(jump);
+    mario.classList.add('jump');
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
